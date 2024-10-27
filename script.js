3916ee3012f7960af18f66da5c4479a3c629d5f0
Daniela <nam991019@gmail.com>
2024-10-27 15:38:24 -0500

    ingresahora

--- script.js
+++ script.js
@@ -2,4 +2,10 @@
 let fecha =parseFloat(prompt("ingresa al fecha"))
 let hora = parseFloat(prompt("ingresa la hora"))
 
-if 
+if (hora >=5 && hora <12) {
+    saludo = "¡Buenos Días!";
+} else if (hora >=12 && hora <18) {
+    saludo = "Buenas Tardes!";
+} else {
+    saludo 
+}
