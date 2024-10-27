cf47b13e0405ae0796550c88bb04136c44547439
Daniela <nam991019@gmail.com>
2024-10-27 16:57:06 -0500

    respositorio hora

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 //Saludo segun hora del dia//
-function SaludarUsuario() {
-    const fecha = new Date();
+   
+const fecha = new Date();
     const hora = fecha.getHours();
-}
-let saludo;
-if (hora >=5 && hora <12) {
+    let saludo = parseInt(prompt("ingresa la hora"))
+
+
+if (hora < 12) {
     saludo = "¡Buenos Días!";
-} else if (hora >=12 && hora <18) {
+} else if (hora < 18) {
     saludo = "Buenas Tardes!";
 } else {
     saludo = "¡Buenas Noches!";
 }
 //Mostrar saludo//
-document.write ('Saludo');
+document.write (saludo);
